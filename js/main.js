a773424d7b9ab1c4396215f53869204e538fd4f1
David Olivari <david.olivari@gmail.com>
2016-02-18 14:55:21 +0100

    Text print working

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function() {
-	
+    popcorn = Popcorn('#myAudio');
+    $.get('data/jnaje.json', function(data) {
+        $.each(data, function(i, obj) {
+        	popcorn.cue(obj.position-.5,function(){
+        		$('#text').text(obj.word);
+        	});
+        });
+    });
 });
