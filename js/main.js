2b205b33cd9ac834f15cce45228014b6ba52f360
David Olivari <david.olivari@gmail.com>
2016-02-26 10:19:35 +0100

    beta version

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 $(function() {
-    popcorn = Popcorn('#myAudio');
-    $.get('data/jnaje.json', function(data) {
-        $.each(data, function(i, obj) {
-        	popcorn.cue(obj.position-.5,function(){
-        		$('#text').text(obj.word);
-        	});
-        });
+  popcorn = Popcorn('#myAudio');
+  $.get('http://api.bype.org/jnaje.json', function(data) {
+    $.each(data, function(i, obj) {
+      popcorn.cue(obj.position - .5, function() {
+        $('#word').text(obj.word);
+        twttr.widgets.createTweet(
+          obj.twitt,
+          document.getElementById('container'), { conversation: 'none' }
+        );
+        $('html,body').animate({
+          scrollTop: $('#container').height()+200
+        }, 2000, 'swing');
+      });
     });
+  });
 });
