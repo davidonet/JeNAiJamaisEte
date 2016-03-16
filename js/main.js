9af6e2ec83da3e35a69a4ef9542d807599203ee7
David Olivari <david.olivari@gmail.com>
2016-03-16 10:46:29 +0100

    Beta version with dark background

--- js/main.js
+++ js/main.js
@@ -3,15 +3,19 @@ $(function() {
   $.get('http://api.bype.org/jnaje.json', function(data) {
     $.each(data, function(i, obj) {
       popcorn.cue(obj.position - .5, function() {
-        $('#word').text(obj.word);
+        $('#container').append('<div class="word">'+obj.word+'</div>');
         twttr.widgets.createTweet(
           obj.twitt,
-          document.getElementById('container'), { conversation: 'none' }
+          document.getElementById('container'), 
+          { conversation: 'none' }
         );
         $('html,body').animate({
           scrollTop: $('#container').height()+200
         }, 2000, 'swing');
       });
     });
+  });
+  popcorn.cue(1,function(){
+  	$("#caution").fadeOut(20000);
   });
 });
