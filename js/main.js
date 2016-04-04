a950c46edd1b0994d89035d4e1f03f222310e5c6
David Olivari <david.olivari@gmail.com>
2016-04-04 11:20:24 +0200

    adding cube in bgq

--- js/main.js
+++ js/main.js
@@ -1,13 +1,57 @@
+/*global $:true Popcorn:true twttr:true*/
+
 $(function() {
-  popcorn = Popcorn('#myAudio');
+
+  $.popcorn = Popcorn('#myAudio');
+
+  $.popcorn.cue(1, function() {
+    $('#caution').fadeOut(20000);
+  });
+
+  $.popcorn.cue(232, function() {
+    $('#credits').fadeIn();
+  });
+
+  function InterVideo(id) {
+    var videoelt = $(id);
+    return {
+      start: function() {
+        $('#container').fadeOut(1000, function() {
+          videoelt[0].play();
+          videoelt.fadeIn(1000);
+        });
+      },
+      stop: function() {
+        videoelt.fadeOut(2000, function() {
+          videoelt[0].pause();
+          $('#container').fadeIn(1000);
+          window.scrollTo(0, document.body.scrollHeight);
+        });
+      }
+    };
+  }
+  var refrain1 = new InterVideo('#refrain1');
+  var refrain2 = new InterVideo('#refrain2');
+  var finidetre = new InterVideo('#finidetre');
+
+  $.popcorn.cue(68, refrain1.start);
+  $.popcorn.cue(105, refrain1.stop);
+  $.popcorn.cue(150, refrain2.start);
+  $.popcorn.cue(160, refrain2.stop);
+  $.popcorn.cue(207, finidetre.start);
+  $.popcorn.cue(230, finidetre.stop);
+
+
   $.get('http://api.bype.org/jnaje.json', function(data) {
     $.each(data, function(i, obj) {
-      popcorn.cue(obj.position - .5, function() {
+      $.popcorn.cue(obj.position - .5, function() {
         $('#container').append('<div class="word">' + obj.word + '</div>');
         var n = Math.floor(Math.random() * (obj.twitt.length - 1));
         twttr.widgets.createTweet(
           obj.twitt[n],
-          document.getElementById('container'), { conversation: 'none' }
+          document.getElementById('container'), {
+            conversation: 'none'
+          }
         );
         $('html,body').animate({
           scrollTop: $('#container').height() + 200
@@ -15,33 +59,5 @@ $(function() {
       });
     });
   });
-  popcorn.cue(1, function() {
-    $("#caution").fadeOut(20000);
-  });
-  popcorn.cue(250, function() {
-    $('html,body').animate({
-      scrollTop: $('#container').height() + 200
-    }, 2000, 'swing', function() {
-      $("#credits").fadeIn(5000);
-    });
-  });
-
-  var startRefrain = function() {
-    $("#vidcub")[0].play();
-    $("#bis").fadeIn(5000)
-  }
-
-  var stopRefrain = function() {
-    $("#bis").fadeOut(5000, function() {
-      $("#vidcub")[0].pause();
-    })
-  }
-
-  popcorn.cue(72, startRefrain);
-  popcorn.cue(105, stopRefrain);
-  popcorn.cue(150, startRefrain);
-  popcorn.cue(160, stopRefrain);
-  popcorn.cue(210, startRefrain);
-  popcorn.cue(240, stopRefrain);
 
 });
